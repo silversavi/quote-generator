e50472b77cf2e3c715dbc0bf604ca77d62a96647
Silver Savi <silver.savi@gmail.com>
2020-08-25 22:57:34 +0300

    finished loader, fetch and styling

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
-// Get Quote From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote From API
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    showLoadingSpinner();
+    const proxyUrl = 'https://blooming-forest-38483.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
+
+        // If author is blank, add 'unknown'
+        if (data.quoteAuthor === "") {
+            authorText.innerText = "unknown"
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
         console.log(data);
+        
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+
+        // Stop loader and show quote
+        removeLoadingSpinner();
     } catch (error) {
-        //getQuote();
-        console.log('miski läks valesti', error);
+        console.log('error')
     }
 }
 
-//On Load
 
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// On Load
 getQuote();
